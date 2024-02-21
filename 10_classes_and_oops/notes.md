c2ddcae6e785254dc41119593f323a6460291a97
Pawankamal16 <95081287+Pawankamal16@users.noreply.github.com>
2024-02-21 09:52:43 +0000

    commit

---
+++ 10_classes_and_oops/notes.md
@@ -0,0 +1,34 @@
+# javascript and classes
+
+//technically classes is not inside javascript,is progg style
+   
+//programming style may be oops and functional
+
+//constructor method is a special method for cretaing and intializing an object
+
+## oops
+
+oops is a programming paradise means what is the structure of writing your code,what style you do prefer
+
+## object
+ - collection pf properties(variable) and method
+ - toLowerCase,toUpperCase, date, promise etc are objects
+
+## why use oops
+
+ code has much mashup before oops, in oops we use feature like serives and you can take its instances later
+
+## parts of oops
+
+object literal
+- constructer
+- prototypes
+- classes
+- instances (new,this)
+
+
+## 4 pillars
+- Abstraction  // hide the internal details,fetch remove all the details from you
+- Encapsulation  //wrap the data and function together,give the acces whatever i want to do or vice versa
+- Inheritance
+- Polymorphism  // one methods can many work
---
+++ 10_classes_and_oops/oop.js
@@ -0,0 +1,43 @@
+// const user = {
+
+// }
+// literally object, in js called base unit
+
+
+//object literal - collection of properties and method
+//this keyboard tells the current context
+const user = {
+   username : "pawan",
+   loggedIn : true,
+   loginCount : 9,
+
+   getUserDetails: function(){
+    console.log("Got user details from database");
+    console.log(`Username: ${this.username}`);
+    console.log(this);
+}
+}
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
+
+
+//new is a constructor function h, ek hi object se multiple constructor bna skte h
+//jaise hi this object create krenege, ek new empty {} object create hoga.then constuctor func call hoga due to this new keywords and print arguments
