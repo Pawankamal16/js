54d29f8fe0bb47068f17ac733940d96d365572b6
Pawankamal16 <95081287+Pawankamal16@users.noreply.github.com>
2024-03-09 07:36:40 +0000

    commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -53,8 +53,9 @@ let str3 = str1 + str2
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true);   // 1
+// console.log(true+);   //error
+// console.log(+"");    //0
 
 let num1, num2, num3
 
