96694abc1affb5ef1238da332195b23603d7e016
Pawankamal16 <95081287+Pawankamal16@users.noreply.github.com>
2024-03-09 10:02:39 +0000

    commit

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -8,7 +8,7 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0]);
-// console.log(gameName.__proto__);
+// console.log(gameName.__proto__);   // {}
 
 
 // console.log(gameName.length);
@@ -24,7 +24,7 @@ console.log(anotherString);
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());   //remove the spaces
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -1,22 +1,22 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);    // object
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());  //gives value in milisecond
-// console.log(Math.floor(Date.now()/1000));  //gives value in second
+console.log(myTimeStamp);   // in milisecond
+console.log(myCreatedDate.getTime());  //gives value in milisecond
+console.log(Math.floor(Date.now()/1000));  //gives value in second
 
 let newDate = new Date()
 console.log(newDate);
